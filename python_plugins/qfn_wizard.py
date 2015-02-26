fccdf58729eeed3f15245902007a6b7e2527e16d
natsfr <nats.fr@gmail.com>
2015-02-26 01:05:09 +0100

    silkscreen bug + IO file error

--- python_plugins/qfn_wizard.py
+++ python_plugins/qfn_wizard.py
@@ -20,6 +20,8 @@ class QFNWizard(HFPW.HelpfulFootprintWizardPlugin):
         self.AddParam("Pads", "oval", self.uBool, True)
         self.AddParam("Pads", "Width", self.uMM, 4)
         self.AddParam("Pads", "Length", self.uMM, 4)
+        self.AddParam("Pads", "Fillet", self.uMM, 0.3);
+
         self.AddParam("TPad", "tpad", self.uBool, True)
         self.AddParam("TPad", "W", self.uMM, 2.6)
         self.AddParam("TPad", "L", self.uMM, 2.6)
@@ -50,32 +52,42 @@ class QFNWizard(HFPW.HelpfulFootprintWizardPlugin):
         #we add 0.3mm to the pad size (to be a parameter)
         #those 0.3mm will be outside of the package
         pad_len = pads["pad length"]
-        fillet = pcbnew.FromMM(0.3)
-        size_2 = pads["Length"] / 2
+        fillet = pads["Fillet"]
+        pad_total = pad_len + fillet
+        len_2 = pads["Length"] / 2
+        wid_2 = pads["Width"] / 2
 
-        pad_center = (size_2 - pad_len + (pad_len+fillet)/2)
+        pad_center_l = (len_2 - pad_len + (pad_len+fillet)/2)
+        pad_center_w = (wid_2 - pad_len + (pad_len+fillet)/2)
 
-        v_pad = PA.PadMaker(self.module).SMDPad(pads["pad width"], pads["pad length"], pcbnew.PAD_RECT)
-        h_pad = PA.PadMaker(self.module).SMDPad(pads["pad length"], pads["pad width"], pcbnew.PAD_RECT)
+        shape = pcbnew.PAD_OVAL if pads["*oval"] else pcbnew.PAD_RECT
 
-        pin1pos = pcbnew.wxPoint(-pad_center, 0)
+        v_pad = PA.PadMaker(self.module).SMDPad(pads["pad width"], pad_total, shape=shape)
+        h_pad = PA.PadMaker(self.module).SMDPad(pad_total, pads["pad width"], shape=shape)
+
+        pin1pos = pcbnew.wxPoint(-pad_center_l, 0)
         array = PA.PadLineArray(v_pad, nb_pads_row, pads["pitch"], True, pin1pos)
         array.SetFirstPadInArray(1)
         array.AddPadsToModule(self.draw)
 
-        pin1pos = pcbnew.wxPoint(0, pad_center)
+        pin1pos = pcbnew.wxPoint(0, pad_center_w)
         array = PA.PadLineArray(h_pad, nb_pads_row, pads["pitch"], False, pin1pos)
         array.SetFirstPadInArray(int(nb_pads_row + 1))
         array.AddPadsToModule(self.draw)
 
-	pin1pos = pcbnew.wxPoint(pad_center, 0)
-	array = PA.PadLineArray(v_pad, nb_pads_row, -pads["pitch"], True, pin1pos)
-	array.SetFirstPadInArray(int(nb_pads_row*2 + 1))
-	array.AddPadsToModule(self.draw)
+        pin1pos = pcbnew.wxPoint(pad_center_l, 0)
+        array = PA.PadLineArray(v_pad, nb_pads_row, -pads["pitch"], True, pin1pos)
+        array.SetFirstPadInArray(int(nb_pads_row*2 + 1))
+        array.AddPadsToModule(self.draw)
 
-	pin1pos = pcbnew.wxPoint(0, -pad_center)
+        pin1pos = pcbnew.wxPoint(0, -pad_center_w)
         array = PA.PadLineArray(h_pad, nb_pads_row, -pads["pitch"], False, pin1pos)
         array.SetFirstPadInArray(int(nb_pads_row*3 + 1))
         array.AddPadsToModule(self.draw)
+
+        self.draw.BoxWithDiagonalAtCorner(0, 0, pads["Length"], pads["Width"], 1)
+        text_size = pcbnew.FromMM(1.2)
+        self.draw.Value(0, pads["Width"] + fillet, text_size)
+        self.draw.Reference(0, -pads["Width"] - fillet, text_size)
 
 QFNWizard().register()
